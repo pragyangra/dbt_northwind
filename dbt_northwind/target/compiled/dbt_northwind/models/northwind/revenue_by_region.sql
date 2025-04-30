b7b8fc5f68724da31c2c9a763bb9badf03011a94
pragyangra <61738363+pragyangra@users.noreply.github.com>
2025-04-30 21:38:27 +0530

    Added the dbt project files here
    This includes all the required files used to dbt model creation.

---
+++ dbt_northwind/target/compiled/dbt_northwind/models/northwind/revenue_by_region.sql
@@ -0,0 +1,15 @@
+-- models/northwind/revenue_by_region.sql
+WITH revenue_by_region AS (
+  SELECT
+    o.ship_region,
+    COUNT(o.order_id) AS num_orders,
+    SUM(od.unit_price * od.quantity * (1 - od.discount)) AS total_sales
+  FROM
+    "postgres"."public"."orders" o
+    LEFT JOIN "postgres"."public"."order_details" od ON o.order_id = od.order_id
+  GROUP BY
+    o.ship_region
+)
+
+SELECT *
+FROM revenue_by_region
